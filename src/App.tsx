dcbd6f99d7ad663cf48efbc4ba9c5c077f4eda12
brandonbohn <brandonbohn@yahoo.com>
2026-01-20 04:16:23 +0000

    Fix black screen flash on initial load - add immediate background color

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
 
 
-    <div>
+    <div style={{ minHeight: '100vh', backgroundColor: '#0b0b0b' }}>
       <div style={{ paddingTop: '20px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
